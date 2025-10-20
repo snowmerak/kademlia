55b1dc0e764b532ec54bb371a375947fb12f4054
snowmerak <snowmerak@outlook.com>
2025-10-20 21:59:34 +0900

    feat(kademlia): add GetBucketIndex method to IDHasher interface and implementations

--- hashed_id.go
+++ hashed_id.go
@@ -2,12 +2,15 @@ package kademlia
 
 import (
 	"crypto/sha1"
+	"encoding/binary"
+	"math/bits"
 
 	"lukechampine.com/blake3"
 )
 
 type IDHasher interface {
 	Hash(data []byte) []byte
+	GetBucketIndex(myID, otherID []byte) int
 }
 
 type LegacyIDHasher struct{}
@@ -17,9 +20,74 @@ func (h *LegacyIDHasher) Hash(data []byte) []byte {
 	return hashed[:]
 }
 
+func (h *LegacyIDHasher) GetBucketIndex(myID, otherID []byte) int {
+	var distance [20]byte
+	var lz int
+
+	for i := 0; i < 20; i++ {
+		distance[i] = myID[i] ^ otherID[i]
+	}
+
+	b0 := binary.BigEndian.Uint64(distance[0:8])
+	if b0 != 0 {
+		lz = bits.LeadingZeros64(b0)
+	} else {
+		b1 := binary.BigEndian.Uint64(distance[8:16])
+		if b1 != 0 {
+			lz = 64 + bits.LeadingZeros64(b1)
+		} else {
+			b2 := binary.BigEndian.Uint32(distance[16:20])
+			if b2 != 0 {
+				lz = 128 + bits.LeadingZeros32(b2)
+			} else {
+				return -1
+			}
+		}
+	}
+
+	bucketIndex := 159 - lz
+
+	return bucketIndex
+}
+
 type ExtendedIDHasher struct{}
 
 func (h *ExtendedIDHasher) Hash(data []byte) []byte {
 	hashed := blake3.Sum256(data)
 	return hashed[:]
+}
+
+func (h *ExtendedIDHasher) GetBucketIndex(myID, otherID []byte) int {
+	var distance [32]byte
+	var lz int
+
+	for i := 0; i < 32; i++ {
+		distance[i] = myID[i] ^ otherID[i]
+	}
+
+	b0 := binary.BigEndian.Uint64(distance[0:8])
+	if b0 != 0 {
+		lz = bits.LeadingZeros64(b0)
+	} else {
+		b1 := binary.BigEndian.Uint64(distance[8:16])
+		if b1 != 0 {
+			lz = 64 + bits.LeadingZeros64(b1)
+		} else {
+			b2 := binary.BigEndian.Uint64(distance[16:24])
+			if b2 != 0 {
+				lz = 128 + bits.LeadingZeros64(b2)
+			} else {
+				b3 := binary.BigEndian.Uint64(distance[24:32])
+				if b3 != 0 {
+					lz = 192 + bits.LeadingZeros64(b3)
+				} else {
+					return -1
+				}
+			}
+		}
+	}
+
+	bucketIndex := 255 - lz
+
+	return bucketIndex
 }
