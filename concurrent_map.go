49597ac6867686006664ca70c9c287ea99f1f856
snowmerak <snowmerak@outlook.com>
2025-10-21 00:49:24 +0900

    feat(concurrent_map): implement concurrent map with generic key-value storage

---
+++ concurrent_map.go
@@ -0,0 +1,57 @@
+package kademlia
+
+import "sync"
+
+type ConcurrentMap[K, V any] struct {
+	m sync.Map
+}
+
+func NewConcurrentMap[K, V any]() *ConcurrentMap[K, V] {
+	return &ConcurrentMap[K, V]{}
+}
+
+func (cm *ConcurrentMap[K, V]) Load(key K) (V, bool) {
+	value, ok := cm.m.Load(key)
+	if !ok {
+		var zero V
+		return zero, false
+	}
+
+	v, ok := value.(V)
+	return v, ok
+}
+
+func (cm *ConcurrentMap[K, V]) Store(key K, value V) {
+	cm.m.Store(key, value)
+}
+
+func (cm *ConcurrentMap[K, V]) Delete(key K) {
+	cm.m.Delete(key)
+}
+
+func (cm *ConcurrentMap[K, V]) Swap(key K, newValue V) (oldValue V, loaded bool) {
+	value, loaded := cm.m.Swap(key, newValue)
+	if !loaded {
+		var zero V
+		return zero, false
+	}
+
+	v, ok := value.(V)
+	if !ok {
+		var zero V
+		return zero, false
+	}
+
+	return v, true
+}
+
+func (cm *ConcurrentMap[K, V]) Range(f func(key K, value V) bool) {
+	cm.m.Range(func(k, v any) bool {
+		key, ok1 := k.(K)
+		value, ok2 := v.(V)
+		if !ok1 || !ok2 {
+			return true
+		}
+		return f(key, value)
+	})
+}
