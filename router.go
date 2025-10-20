89f50db4255e37221b8bf43cfceb2184ee8368f7
snowmerak <snowmerak@outlook.com>
2025-10-20 23:41:28 +0900

    feat(kademlia): add methods for node management and retrieval in Router

--- router.go
+++ router.go
@@ -1,6 +1,8 @@
 package kademlia
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Config struct {
 	KeyExchanger KeyExchanger
@@ -76,4 +78,114 @@ func (r *Router) Initialize(force bool) error {
 	}
 
 	return nil
+}
+
+func (r *Router) ID() []byte {
+	id := make([]byte, len(r.id))
+	copy(id, r.id)
+	return id
+}
+
+func (r *Router) PublicKey() ([]byte, error) {
+	pubKey, err := r.store.GetPublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get public key from store: %w", err)
+	}
+
+	return pubKey, nil
+}
+
+func (r *Router) Handshake(peerPublicKey []byte) ([]byte, error) {
+	privKey, err := r.store.GetPrivateKey()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get private key from store: %w", err)
+	}
+
+	sharedSecret, err := r.keyExchanger.ComputeSharedSecret(privKey, peerPublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compute shared secret: %w", err)
+	}
+
+	return sharedSecret, nil
+}
+
+func (r *Router) FindNode(targetID []byte) (*Contact, error) {
+	idx := r.hasher.GetBucketIndex(r.id, targetID)
+	data, err := r.store.GetNodeFromBucket(idx, targetID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find node in store: %w", err)
+	}
+
+	c := &Contact{}
+	if err := c.Unmarshal(data); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal contact: %w", err)
+	}
+
+	return c, nil
+}
+
+func (r *Router) StoreNode(c *Contact) error {
+	data := c.Marshal()
+	idx := r.hasher.GetBucketIndex(r.id, c.ID)
+	if err := r.store.AddNodeToBucket(idx, c.ID, data); err != nil {
+		return fmt.Errorf("failed to store contact in store: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Router) FindNearbyNodes(targetID []byte, count int) ([]*Contact, error) {
+	idx := r.hasher.GetBucketIndex(r.id, targetID)
+	originIdx := idx
+
+	result := []*Contact{}
+	for idx >= 0 && len(result) < count {
+		contactsData, err := r.store.GetAllNodesInBucket(idx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get nodes from bucket: %w", err)
+		}
+
+		for _, data := range contactsData {
+			if len(result) >= count {
+				break
+			}
+
+			c := &Contact{}
+			if err := c.Unmarshal(data); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal contact: %w", err)
+			}
+
+			result = append(result, c)
+		}
+
+		idx--
+	}
+
+	if len(result) < count {
+		idx = originIdx + 1
+		maxIndex := r.hasher.MaxIDLength()
+		for len(result) < count && idx < maxIndex {
+			contactsData, err := r.store.GetAllNodesInBucket(idx)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get nodes from bucket: %w", err)
+			}
+
+			for _, data := range contactsData {
+				if len(result) >= count {
+					break
+				}
+
+				c := &Contact{}
+				if err := c.Unmarshal(data); err != nil {
+					return nil, fmt.Errorf("failed to unmarshal contact: %w", err)
+				}
+
+				result = append(result, c)
+			}
+
+			idx++
+		}
+	}
+
+	return result, nil
 }
