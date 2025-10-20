4297b27100eda587528f7736b3cd2c57e52a1607
snowmerak <snowmerak@outlook.com>
2025-10-20 23:19:52 +0900

    feat(kademlia): implement Initialize method for Router to manage key pair generation and storage

--- router.go
+++ router.go
@@ -28,3 +28,52 @@ func NewRouter(id []byte, config Config) (*Router, error) {
 		store:        strg,
 	}, nil
 }
+
+func (r *Router) Close() error {
+	return r.store.Close()
+}
+
+func (r *Router) Initialize(force bool) error {
+	id, err := r.store.GetNodeID()
+	if err != nil {
+		return fmt.Errorf("failed to get node ID from store: %w", err)
+	}
+
+	privKey, err := r.store.GetPrivateKey()
+	if err != nil {
+		return fmt.Errorf("failed to get private key from store: %w", err)
+	}
+
+	pubKey, err := r.store.GetPublicKey()
+	if err != nil {
+		return fmt.Errorf("failed to get public key from store: %w", err)
+	}
+
+	if id != nil && privKey != nil && pubKey != nil && !force {
+		r.id = id
+		return nil
+	}
+
+	priveKey, pubKey, err := r.keyExchanger.GenerateNewKeyPair()
+	if err != nil {
+		return fmt.Errorf("failed to generate new key pair: %w", err)
+	}
+
+	hashedID := r.hasher.Hash(pubKey)
+
+	if err := r.store.SetNodeID(hashedID); err != nil {
+		return fmt.Errorf("failed to save node ID to store: %w", err)
+	}
+
+	r.id = hashedID
+
+	if err := r.store.SetPrivateKey(priveKey); err != nil {
+		return fmt.Errorf("failed to save private key to store: %w", err)
+	}
+
+	if err := r.store.SetPublicKey(pubKey); err != nil {
+		return fmt.Errorf("failed to save public key to store: %w", err)
+	}
+
+	return nil
+}
